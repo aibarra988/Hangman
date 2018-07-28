961f533958a7e71c6f5e6043790d92cf4896ba1c
Abraham Ibarra <aibarra988@gmail.com>
2018-07-28 00:25:04 -0500

    it works now! im going to bed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,43 @@
 // Refactoring idea: use reduce to determine letter matches in the magic word
 
+var hintEl = document.getElementById("hint");
 var stageEl = document.getElementById("stage");
 var winsEl = document.getElementById("wins");
 var guessCounterEl = document.getElementById("guess-counter");
 var attemptsEl = document.getElementById("attempts");
+var loseMessageEl = document.getElementById("lose-message");
+var winSound = new Audio("assets/audio/my_mac.mp3");
+var loseSound = new Audio("assets/audio/windows-95-error-sound-effect.mp3");
 
 var game = {
     attempts: [],
-    wordList: ["Playstation", "Windows 95", "Solo Cups"],
+    wordList: [{
+        word: "A E S T H E T I C",
+        hint: "The driving aspect of vaporwave"
+    }, {
+        word: "Playstation",
+        hint: "Sony's big console debut"
+    }, {
+        word: "Windows 95", 
+        hint: "Microsoft's big hit after 3.1"
+    }, {
+        word: "Solo Cups",
+        hint: "disposable cup with the sweet pattern"
+    }, {
+        word: "Capitalism",
+        hint: "The inescapable machine that governs us all"
+    }, {
+        word: "Michaelangelo",
+        hint: "Greek statue a e s t h e t i c icon"
+    }, {
+        word: "Crystal Pepsi",
+        hint: "Iconic clear soda of the 1990s"
+    }],
     magicWord: "",
     setNewMagicWord: function() {
-        this.magicWord = this.wordList[Math.floor(Math.random() * Math.floor(this.wordList.length))];
+        var randomPick = Math.floor(Math.random() * Math.floor(this.wordList.length));
+        this.magicWord = this.wordList[randomPick].word;
+        this.hint = this.wordList[randomPick].hint;
     },
     progress: [],
     wins: 0,
@@ -33,12 +60,13 @@ var game = {
 
         // If the failed guess isn't already in our attempts list,
         // add guess to attempts and decrease number of guesses remaining
-        if (!match && !(this.attempts.includes(lcGuess))) {
+        if (!(match) && !(this.attempts.includes(lcGuess))) {
             this.attempts.push(guess);
         } 
 
         return match;
     },
+    gameOver: false,
     resetAttempts: function() {
         var self = this;
         this.attempts.forEach(function(letter, index) {
@@ -52,13 +80,15 @@ var game = {
         this.setProgress();
         this.resetAttempts();
         this.guessCounter = 5;
+        loseMessageEl.classList.remove("unhide");
     },
     setProgress: function() {
         for (var i = 0; i < this.magicWord.length; i++) {
             this.progress.push("-");
         }
     },
-    render: function(str) {
+    render: function() {
+        hintEl.textContent = this.hint;
         stageEl.textContent = this.progress.join("");
         winsEl.textContent = this.wins;
         guessCounterEl.textContent = this.guessCounter;
@@ -66,6 +96,7 @@ var game = {
     }
 };
 
+// reset button's onclick handler
 function resetGame() {
     game.resetGame();
     game.render();
@@ -84,7 +115,6 @@ document.onkeyup = function(event) {
     
     // If our progress matches up to our magic word, we have a winner!
     var winner = progress.replace("-", userGuess) === game.magicWord;
-    var gameOver = false;
 
     // Validate user input
     var validInput = /^[a-zA-Z_0-9\s-]$/.test(userGuess);
@@ -92,9 +122,12 @@ document.onkeyup = function(event) {
     if (validInput) {
         
         if (winner) {
-            console.log("woohoo!");
+            // play a sound
+            winSound.play();
+            //show that you're a winner!
             game.findMatch(userGuess);
             game.wins++;
+            alert("Congratulations! The word is: " + game.magicWord);
             game.resetGame();
         // check if the user has available guesses remaining 
         // and the magic word has not been revealed
@@ -106,20 +139,21 @@ document.onkeyup = function(event) {
     
 
             // if the user didn't get a match
-            if (!match && !(game.attempts.includes(userGuess))) {
+            if (!match) {
                 // decrease the number of guesses remaining
                 game.guessCounter--;
-
+                
                 // WHY DOES THIS WORK?!?!?!
-                if (game.guessCounter === 1) {
+                if (!match) {
                     game.gameOver = true;
                 }
             }
 
     
         } else if (game.gameOver) {
-            document.write("Game Over! Try Again?");
+            loseSound.play();
             //then unhide reset button
+            loseMessageEl.classList.add("unhide");
         }
     }
 
